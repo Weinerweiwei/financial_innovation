e507a64889a2505b184cfc1cc1a8d3ff36f0ee23
Weinerweiwei <71777179+Weinerweiwei@users.noreply.github.com>
2020-12-24 15:34:42 +0800

    Add files via upload

---
+++ HW1: collect at least two customized rule-based patterns with indices/Main_r08723062_劉瑋.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from random import sample 
+
+#from Process import Process
+#from Detect import Detect
+#from util.util_pattern import pattern
+
+pd.set_option('display.max_columns', 1000)
+© 2020 GitHub, Inc.
